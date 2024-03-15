38185a8319326493fe174903c1417b82f5d8054a
Carlos Salinas <salinas.carlos14@gmail.com>
2024-03-14 22:30:40 -0400

    Working on implementing favoriting images

--- backend/index.ts
+++ backend/index.ts
@@ -6,12 +6,23 @@ import loginRouter from './routes/login';
 import 'express-async-errors';
 import middleware from './utils/middleware';
 import dotenv from 'dotenv';
+import { UserToken } from './types';
 dotenv.config();
 //import { connectToDatabase } from './utils/db';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+//used to expand our express request properties
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      user?: UserToken;
+    }
+  }
+}
 
 app.use('/api/dogapi', dogApiRouter);
 app.use('/api/users', userRouter);
--- backend/utils/middleware.ts
+++ backend/utils/middleware.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Response, Request } from 'express';
 import logger from './logger';
+import * as jwt from 'jsonwebtoken';
+import { SECRET } from '../utils/config';
+import { Session } from '../models';
+import { UserToken } from '../types';
 
 const errorHandler = (
   error: Error,
@@ -19,6 +23,43 @@ const errorHandler = (
   next(error);
 };
 
+const tokenExtractor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const authorization = request.get('authorization');
+  console.log(authorization);
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      const decodedToken = jwt.verify(authorization.substring(7), SECRET);
+      console.log(decodedToken);
+
+      //check if this token is an active session in our db
+
+      //####fix up error checking for active sessions. If not found its triggering 'token invalid'
+      const activeSession = await Session.findOne({
+        where: {
+          token: authorization.substring(7)
+        }
+      });
+      console.log(activeSession);
+      if (!activeSession) {
+        return response.status(401).json({ error: 'active session not found' });
+      }
+
+      console.log(decodedToken);
+      request.user = decodedToken as UserToken;
+    } catch {
+      return response.status(401).json({ error: 'token invalid' });
+    }
+  } else {
+    return response.status(401).json({ error: 'token missing' });
+  }
+  return next();
+};
+
 export default {
-  errorHandler
+  errorHandler,
+  tokenExtractor
 };
