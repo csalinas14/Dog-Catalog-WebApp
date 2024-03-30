7940228fa9c1fd33d00fdcfa0a440058dc5f74b2
Carlos Salinas <salinas.carlos14@gmail.com>
2024-03-29 21:42:24 -0400

    Getting carousel to align with pictures properly and work when window is resized #minor

---
+++ frontend/src/components/LandingPage/Ending/index.tsx
@@ -0,0 +1,13 @@
+const LandingPageEnding = () => {
+  return (
+    <div className='bg-base-200 py-20 px-8 sm:px-12 lg:px-28'>
+      <hr className='block w-48 h-1 border-0 bg-accent rounded-md mb-8' />
+      <h2 className='flex flex-wrap text-xl font-semibold mb-8 lg:text-3xl lg:font-bold xl:text-5xl'>
+        Getting
+        <span className='text-accent'> &nbsp;started</span>
+      </h2>
+    </div>
+  )
+}
+
+export default LandingPageEnding
